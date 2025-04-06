2678a3c7d5eb82e86c6688e88807a5aa86ae7f78
Daniel Otero <jdanielotero01@gmail.com>
2025-04-05 23:47:29 -0500

    Font Awesome

--- includes/header.php
+++ includes/header.php
@@ -6,13 +6,16 @@
   <title>PHP MYSQL CRUD</title>
   <!-- Bootstrao 5.3 -->
   <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
-  <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/bootstrap/bootstrap.min.css" rel="stylesheet">
   <!-- Font Awesome -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
- </head>
+  <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"> -->
+  <link href="assets/font-awesome/font-awesome.min.js" rel="stylesheet">
+</head>
 <body>
-    <nav class="navbar navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">PHP MySQL CRUD</a>
-      </div>
-    </nav>
+  <nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="index.php">
+        <i class="fas fa-database me-2"></i> <i class="far fa-camera me-2"></i>PHP MySQL CRUD
+      </a>
+    </div>
+  </nav>
